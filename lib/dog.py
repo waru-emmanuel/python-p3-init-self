865ce8630ab57197e74c11651ac42a602aca16ec
EMMANUEL <waruterewachira7@gmail.com>
2024-07-02 02:08:44 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    '''Dog in dog.py'''
+
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+    def get_owner(self):
+        return self.owner
+    
+    
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+    
+
+    # Create an instance of the Person class
+person = Person("John Doe", 30)
+
+# Print the person object
+print(person)
+
